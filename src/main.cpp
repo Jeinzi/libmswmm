1fe70b221e9ac839319e66581b3a36030e0675e4
Jeinzi <jeinzi@gmx.de>
2023-02-15 17:18:56 +0100

    Switch to GPLv2 or later

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
+/*******************************************************************
+libmswmm: Read Microsoft Windows Movie Maker (.mswmm) files.
+Copyright © 2023 Julian Heinzel
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+See LICENCE file for the full license text.
+*******************************************************************/
 #include <memory>
 #include <fstream>
 #include <iostream>
